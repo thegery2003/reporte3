a26460b525828e4674247ebe215db9e250194de1
Brayan Gerardo Montaño Escutia <bmontanoe001@alumno.uaemex,mx>
2022-08-23 22:49:25 -0500

    1

---
+++ src/repositorio.java
@@ -0,0 +1,7 @@
+public class repositorio {
+    public static void main(String[] args) {
+        double n1=10,n2=8,n3;
+        n3=n1+n2;
+        System.out.println("la suma de estos dos numeros es: "+n3);
+    }
+}
